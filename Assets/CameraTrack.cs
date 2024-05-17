dc587270057427073d13bc032e3724105633401f
kle3270 <kle3270@student.pps.net>
2024-05-17 09:56:57 -0700

    camera follow feels so janky but it works + scroll wheel zoom

--- Assets/CameraTrack.cs
+++ Assets/CameraTrack.cs
@@ -4,15 +4,19 @@ public class CameraTrack : MonoBehaviour
 {   
     //Size --> 1/2radius for x, radius for y
     public Rigidbody2D golfBall;
-    public Vector2 maxBoundsCam;
+    public float maxX;
+    public float maxY;
     public float cameraSpeed;
     public Vector3 cameraPanLocation;
+    public Camera camera;
+
  
    // public Camera camera; Will figure this out later to zoom in and out but for now its fine
    //Make camera move at same angle just ofr a little bit too, do cam position - ball position. then inverse tan of y/x 
     void Start()
     {
         cameraPanLocation = new(0, 0, -10);
+        camera.orthographic=true;
     }
 
     Vector2 absValueVectorTwo(Vector2 desiredVector)
@@ -21,13 +25,22 @@ public class CameraTrack : MonoBehaviour
         return absolutedVector;
     }
 
+
     // Update is called once per frame
-    void Update()
+    void Update() 
     {   
+        if(Mathf.Abs(Input.GetAxis("Mouse ScrollWheel"))> 0){
+             camera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel");
+             
+        }
+        Vector2 maxBoundsCam = new Vector2(maxX,maxY) * camera.orthographicSize/5;
+        
+        
         int randomMoveY = Random.Range(1, 5);
-        Vector2 ballDistance = absValueVectorTwo(golfBall.position)-maxBoundsCam+transform.position;
+        Vector2 camPos = new Vector2(transform.position.x,transform.position.y);
+        Vector2 ballDistance = absValueVectorTwo(golfBall.position)-maxBoundsCam+camPos;
         Vector2 notAbsDist = golfBall.position - maxBoundsCam;
-        Debug.Log(ballDistance + ""+ transform.position );
+        Debug.Log(ballDistance + ""+ camPos +maxBoundsCam);
         // y/x = tan(angleFromBall) --> x = y/tan(angleFromBall0
         //This only applies when it is outside of the bounds. It doesnt go to the point, it moves in cameraspeed * deltatime intervals.
         //Try if to update the travel to location and the update just moves it
